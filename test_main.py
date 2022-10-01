7c2c79ab9dd44d566516f1d06b5a2603cc95b056
Ogunsanya James <oyindamolajames@gmail.com>
2022-10-01 17:33:10 +0100

    Test checking against bad dev

--- test_main.py
+++ test_main.py
@@ -15,7 +15,7 @@ PASSWORD = 'huff-puff'
 @pytest.fixture
 def client():
     os.environ['JWT_SECRET'] = SECRET
-    main.APP.config['TESTING'] = False
+    main.APP.config['TESTING'] = True
     client = main.APP.test_client()
 
     yield client
@@ -24,9 +24,9 @@ def client():
 
 def test_health(client):
     response = client.get('/')
-    assert response.status_code == 200
-    assert response.json == 'Healthy'
-
+    assert response.status_code == 400
+    assert response.json == 'Healthyj'
+    
 
 def test_auth(client):
     body = {'email': EMAIL,
